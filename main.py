9cec369893046914823a30ad2e40676c02afee72
Seol-JY <wlsdud5654@gmail.com>
2022-02-21 22:14:25 +0900

    class추가, 매도전략추가, 로그

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
 import pyupbit
-import datetime
 import sys
 import talib
 import numpy as np
+import subprocess
+import datetime
 
 # ----------------------------------------------------------------
 f = open("access.txt", 'r')
@@ -17,6 +18,7 @@ if confirm == "y":
     print("Login Sccess!!")
 else:
     sys.exit("Program exit.")
+logger = subprocess.Popen('python logger.py', creationflags = subprocess.CREATE_NEW_CONSOLE, stdin = subprocess.PIPE)
 print("Autotrading Start...\n")
 # -----------------------------------------------------------------
 
@@ -26,10 +28,11 @@ def get_balance(ticker):
     for b in balances:
         if b['currency'] == ticker:
             if b['balance'] is not None:
+                logger.stdin.write(f"{'Finding Buy signal..' if ticker == 'KRW' else 'Finding Sell signal..'}  Balance: {float(b['balance'])}{ticker}  [{datetime.datetime.now().strftime('%H:%M:%S')}]\n".encode('utf-8'))
+                logger.stdin.flush()
                 return float(b['balance'])
             else:
                 return 0
-
 
 def buy_order(ticker, volume):
     try:
@@ -60,48 +63,43 @@ def sell_order(ticker, volume):
 def log(_dict):
     diction = {"bid": "매수주문", "ask": "매도주문"}
     _created_at = _dict['created_at'][:10] + " " + _dict['created_at'][11:19]
-    print(
-        f"[{_created_at}] {_dict['market']} {diction[_dict['side']]} ({_dict['locked']})")
+    print(f"[{_created_at}] {_dict['market']} {diction[_dict['side']]} ({_dict['locked']})")
 
 
-def now_rsi():
-    df = pyupbit.get_ohlcv("KRW-BTC", interval="minute10", count=200)
-    rsi = talib.RSI(np.asarray(df['close']), 14)
-    return rsi[-1]
+class Indicator:
+    def __init__(self):
+        self.df = pyupbit.get_ohlcv("KRW-BTC", interval="minute10", count=200)
 
+    def now_rsi(self):
+        rsi = talib.RSI(np.asarray(self.df['close']), 14)
+        return rsi[-1]
 
-def now_macdOR():
-    df = pyupbit.get_ohlcv("KRW-BTC", interval="minute10", count=200)
-    macd, signal, hist = talib.MACD(np.asarray(
-        df['close']), fastperiod=8, slowperiod=21, signalperiod=5)
-    return hist[-1]
+    def now_macdOR(self):
+        hist = talib.MACD(np.asarray(self.df['close']), fastperiod=8, slowperiod=21, signalperiod=5)[2][-1]
+        return hist
 
+    def now_stochastic(self):
+        slowk, slowd = talib.STOCH(np.asarray(self.df['high']), np.asarray(self.df['low']), np.asarray(self.df['close']), fastk_period=14, slowk_period=3, slowd_period=3)
+        return [slowk[-1], slowd[-1]]
 
-def now_stochastic():
-    df = pyupbit.get_ohlcv("KRW-BTC", interval="minute10", count=200)
-    slowk, slowd = talib.STOCH(np.asarray(df['high']), np.asarray(df['low']), np.asarray(
-        df['close']), fastk_period=14, slowk_period=3, slowd_period=3)
-    return [slowk[-1], slowd[-1]]
+def bid_check():  #매수조건
+    indicator = Indicator()
+    cnt = True
+    cnt = cnt and (True if indicator.now_stochastic()[0] < 80 and indicator.now_stochastic()[1] < 80 else False)
+    cnt = cnt and (True if indicator.now_rsi() >= 50 else False)
+    cnt = cnt and (True if indicator.now_macdOR() >= 0 else False)
+    return cnt
 
-
-def bid_check():
-    # 매수조건
-    if (now_stochastic()[0] < 80) and (now_stochastic()[1] < 80) and (now_rsi() > 50) and (now_macdOR() > 0):
-        return True
-    else:
-        return False
-
-
-def ask_check():
+def ask_check(): # 매도조건
+    indicator = Indicator()
     cnt = 0
-    cnt += 1 if now_stochastic()[0] > 80 and now_stochastic()[1] > 80 else 0
-    cnt += 1 if now_rsi() <= 50 else 0
-    cnt += 1 if now_macdOR() <= 0 else 0
-    if cnt >= 2:  # 매도조건
+    cnt += 1 if indicator.now_stochastic()[0] >= 80 and indicator.now_stochastic()[1] >= 80 else 0
+    cnt += 1 if indicator.now_rsi() <= 50 else 0
+    cnt += 1 if indicator.now_macdOR() <= 0 else 0
+    if cnt >= 2:
         return True
     else:
         return False
-
 
 while True:
     try:
